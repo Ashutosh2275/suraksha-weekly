a320f3b6f7b8a8ac02f696f5901916d8246ae1f7
Ashutosh Mishra <ashutoshmishra2275@gmail.com>
2026-04-07 00:26:06 +0530

    chore: tighten ignore rules and sync required app files

--- apps/admin-web/src/components/common/Skeleton.tsx
+++
@@ -1,7 +0,0 @@
-interface SkeletonProps {
-  className?: string;
-}
-
-export function Skeleton({ className = "" }: SkeletonProps) {
-  return <div className={`animate-pulse rounded-xl bg-slate-200 ${className}`} />;
-}
--- apps/worker-web/src/components/common/Skeleton.tsx
+++
@@ -1,7 +0,0 @@
-interface SkeletonProps {
-  className?: string;
-}
-
-export function Skeleton({ className = "" }: SkeletonProps) {
-  return <div className={`animate-pulse rounded-lg bg-slate-200 ${className}`} />;
-}
