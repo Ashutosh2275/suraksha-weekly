8bd95a9196778f7689b886787b1ea01bcccad1c2
Ashutosh Mishra <ashutoshmishra2275@gmail.com>
2026-04-17 15:49:01 +0530

    fix: make unified static export build pass for Vercel

--- apps/admin/next.config.mjs
+++ apps/admin/next.config.mjs
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/admin',
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     tsconfigPath: './tsconfig.json',
   },
